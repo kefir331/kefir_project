7d8ca9f6c6b1591b42cb40f7a4d251cc48e34360
kefir331 <Seven1486>
2019-10-09 16:14:11 +0300

    added Procfile

--- src/core/settings.py
+++ src/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_q204#d$sh644b)bf!)2wv7n_je2redzvn5txbci1p)onu9*=t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","127.0.0.1", "https://kefir331.herokuapp.com/"]
 
 
 # Application definition
