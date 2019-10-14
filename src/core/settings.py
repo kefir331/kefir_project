5ce39da4ec6a8740ae31d4924cc19ef958a2f305
kefir331 <Seven1486>
2019-10-14 03:34:36 +0300

    444

--- src/core/settings.py
+++ src/core/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-from dynacnf import settings as _settings
+from dynaconf import settings as _settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
