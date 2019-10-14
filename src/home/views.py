4ac9e66978b69d36ab61e4f0092769726aa837d6
denstan93 <denstan93@gmail.com>
2019-10-14 20:22:30 +0300

    1

--- src/home/views.py
+++ src/home/views.py
@@ -3,7 +3,7 @@ from platform import system
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html", {
+    return render(request, "home/templates/index.html", {
         "body_message": "BuySell",
     })
 # Create your views here.
