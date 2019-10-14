0a642f5624748be7eb26a177cc351d2a42344dea
denstan93 <denstan93@gmail.com>
2019-10-14 20:08:58 +0300

    1

--- mysite/index/views.py
+++ mysite/index/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+##   return HttpResponse(f'Hello, world. {request}')
+
+##from django.http import HttpResponse
+
+
+##def index(request):
+##    return HttpResponse("Hello, world.You' re at the polls index {request}")
 
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse(f"Hello, world. {request}")
+    return HttpResponse("Hello, world.You' re at the polls index {request}")
