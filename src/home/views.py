887ce42c76639be018f422cddb6325ea1989aaa6
kefir331 <Seven1486>
2019-10-20 19:39:15 +0300

    block add

--- src/home/views.py
+++ src/home/views.py
@@ -1,9 +1,37 @@
 from platform import system
+from typing import NamedTuple
 
+from django.core.signing import Signer
 from django.shortcuts import render
+
+
+class TabContent(NamedTuple):
+    content: str
+
+
+def buyers():
+    return {
+        "Buyer": TabContent("Nickname"),
+        "Payment method": TabContent("National Bank"),
+        "Price": TabContent("20000"),
+        "Limit": TabContent("1-10000"),
+    }
+
+
+def sellers():
+    return {
+        "Seller": TabContent("Nickname"),
+        "Payment method": TabContent("National bank"),
+        "Price": TabContent("18500"),
+        "Limit": TabContent("1-15000"),
+    }
+
 
 def index(request):
     return render(request, "index.html", {
+        "buyobject": buyers().items(),
+        "sellobject": sellers().items(),
         "body_message": "BuySell",
+        "Main": "BuySellBitcoins Main",
     })
 # Create your views here.
